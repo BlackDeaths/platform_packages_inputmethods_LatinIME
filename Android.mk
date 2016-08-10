a9d07587a8edb93f90a948c506c93b050a15569f
Blackjubel <blackjubels@gmail.com>
2016-08-10 18:43:27 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -11,6 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 subdirs := common native java tests tools
 include $(call all-named-subdir-makefiles, $(subdirs))
